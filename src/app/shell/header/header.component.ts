3c424c4b2c3d2e0edd40f1e505a5bbbad3539c53
Jaivik Patel <jaivik.patel@thetatechnolabs.com>
2022-12-11 21:13:31 +0530

    -solved routing issue"

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -10,6 +10,9 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   isLeads: boolean = false;
+  isMcaLeads: boolean = false;
+  isMortageLeads: boolean = false;
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -17,15 +20,32 @@ export class HeaderComponent implements OnInit {
       this.isLeads = false;
       if (this.router.url.indexOf('mortgage-leads') > 1) {
         this.isLeads = true;
+        this.isMortageLeads = true;
+        this.isMcaLeads = false
+        
       }
       if (this.router.url.indexOf('mca-leads') > 1) {
+        this.isLeads = true;
+        this.isMcaLeads = true;
+        this.isMortageLeads = false;
+
+      }
+      if (this.router.url.indexOf('contact') > 1) {
         this.isLeads = true;
       }
     });
     if (this.router.url.indexOf('mortgage-leads') > 1) {
       this.isLeads = true;
+      this.isMortageLeads = true;
+      this.isMcaLeads = false;
+      
     }
     if (this.router.url.indexOf('mca-leads') > 1) {
+      this.isLeads = true;
+      this.isMortageLeads = false;
+      this.isMcaLeads = true;
+    }
+    if (this.router.url.indexOf('contact') > 1) {
       this.isLeads = true;
     }
   }
