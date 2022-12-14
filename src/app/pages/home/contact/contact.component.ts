81a6476b2db953d448b399b606e351784f443bcc
Devansh Patel <69197303+Dev7774@users.noreply.github.com>
2022-12-14 21:52:55 +0530

    changes

--- src/app/pages/home/contact/contact.component.ts
+++ src/app/pages/home/contact/contact.component.ts
@@ -10,10 +10,10 @@ declare let Email: any;
 export class ContactComponent implements OnInit {
 
   contactForm = new FormGroup({
-    name: new FormControl('',Validators.required),
-    email: new FormControl('',Validators.required),
-    subject: new FormControl('',Validators.required),
-    message: new FormControl('',Validators.required),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
   });
 
   constructor() { }
@@ -21,17 +21,21 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(){
-    this.contactForm.value;
-    Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "7774devansh@gmail.com",
-      Password : "0246575FACD99D483F7A651691C307816DB2",
-      To : this.contactForm.value.email,
-      From : "7774devansh@gmail.com",
-      Subject : this.contactForm.value.subject,
-      Body : this.contactForm.value.message}
-      ).then( (message:any) => {
-        this,this.contactForm.reset(); } );
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.contactForm.value;
+      Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "7774devansh@gmail.com",
+        Password: "0246575FACD99D483F7A651691C307816DB2",
+        To: this.contactForm.value.email,
+        From: "7774devansh@gmail.com",
+        Subject: this.contactForm.value.subject,
+        Body: this.contactForm.value.message
+      }
+      ).then((message: any) => {
+        this, this.contactForm.reset();
+      });
     }
+  }
 }
